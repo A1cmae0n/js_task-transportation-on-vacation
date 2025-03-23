e37f8c9c6fe76b7dfb636f045ea0def25aa1c322
Your Name <you@example.com>
2025-03-23 21:57:54 +0700

    Changes per checklist

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,22 +5,28 @@
  *
  * @return {number}
  */
-function calculateRentalCost(days) {
+function calculateRentalCost(numberOfDays) {
+  //
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const MID_TERM = 3;
+  const MID_TERM_DISCOUNT = 20;
   // Every day you rent the car costs $40.
-  let total = days * 40;
+  const basePrice = numberOfDays * 40;
 
   // If you rent the car for 7 or more days, you get $50 off your total.
-  if (days >= 7) {
-    return (total -= 50);
+  if (numberOfDays >= LONG_TERM) {
+    return basePrice - LONG_TERM_DISCOUNT;
   }
 
   // Alternatively, if you rent the car for 3 or more days,
   // you get $20 off your total.
-  if (days >= 3) {
-    return (total -= 20);
+  if (numberOfDays >= MID_TERM) {
+    return basePrice - MID_TERM_DISCOUNT;
   }
 
-  return total;
+  // No discount
+  return basePrice;
 }
 
 module.exports = calculateRentalCost;
