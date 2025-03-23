a729e9a5167ff81581b0332b2c15ec8a372609dc
Your Name <you@example.com>
2025-03-23 21:51:43 +0700

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -1,10 +1,26 @@
+'use strict';
+
 /**
  * @param {number} days
  *
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  // Every day you rent the car costs $40.
+  let total = days * 40;
+
+  // If you rent the car for 7 or more days, you get $50 off your total.
+  if (days >= 7) {
+    return (total -= 50);
+  }
+
+  // Alternatively, if you rent the car for 3 or more days,
+  // you get $20 off your total.
+  if (days >= 3) {
+    return (total -= 20);
+  }
+
+  return total;
 }
 
 module.exports = calculateRentalCost;
